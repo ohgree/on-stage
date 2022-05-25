509abe67c27ac6654830e31e5ce2d5228b6ff40e
jagajang <kosb7544@gmail.com>
2022-05-25 19:06:17 +0900

    multiplay and ID

--- Assets/Scripts/PlayerName.cs
+++ Assets/Scripts/PlayerName.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class PlayerNameLookCam : MonoBehaviour
+public class PlayerName : MonoBehaviourPun
 {
+    PhotonView PV;
     GameObject CurrentCamera;
+    public TextMesh Name;
     
     void Start()
     {
+        PV = photonView;
         CurrentCamera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        if(PV.IsMine)
+        {
+            Name.color = Color.white;
+        }
     } 
 
     void Update()
