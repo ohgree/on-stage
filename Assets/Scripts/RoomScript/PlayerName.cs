410ce352a582c5fe5d61180ebb212cfc8dae07bf
jagajang <kosb7544@gmail.com>
2022-05-26 16:23:02 +0900

    progress on login and lobby

---
+++ Assets/Scripts/RoomScript/PlayerName.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerName : MonoBehaviourPun
+{
+    PhotonView PV;
+    GameObject CurrentCamera;
+    public TextMesh Name;
+    
+    void Start() {
+        PV = photonView;
+        CurrentCamera = GameObject.FindGameObjectWithTag("MainCamera");
+
+        if(PV.IsMine) {
+            Name.color = Color.white;
+        }
+        //
+    } 
+
+    void Update() {
+        transform.rotation = CurrentCamera.transform.rotation;
+    }
+}
