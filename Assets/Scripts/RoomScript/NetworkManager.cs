067c0211b46ec793d5f9783ad13a725f785864c1
MJ Shin <ohgree.dev@gmail.com>
2022-06-01 22:25:47 +0900

    Start fixing photon merge errors

--- Assets/Scripts/RoomScript/NetworkManager.cs
+++ Assets/Scripts/RoomScript/NetworkManager.cs
@@ -5,25 +5,25 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
+using Cinemachine;
 
-public class NetworkManager : MonoBehaviourPunCallbacks
-{
-    public CinemachineFreeLook cam;
-    void Start() {
-        //
-    }
+public class NetworkManager : MonoBehaviourPunCallbacks {
+  public CinemachineFreeLook cam;
+  void Start() {
+    //
+  }
 
-    public override void OnJoinedRoom() {
-        GameObject player = PhotonNetwork.Instantiate("Player", new Vector3(0, 1, 10), Quaternion.identity);
-        cam.LookAt = cam.Follow = player.transform;
-    }
+  public override void OnJoinedRoom() {
+    GameObject player = PhotonNetwork.Instantiate("Player", new Vector3(0, 1, 10), Quaternion.identity);
+    cam.LookAt = cam.Follow = player.transform;
+  }
 
-    public void Disconnect() {
-        PhotonNetwork.LeaveRoom();
-        SceneManager.LoadScene("Lobby");
-    }
+  public void Disconnect() {
+    PhotonNetwork.LeaveRoom();
+    SceneManager.LoadScene("Lobby");
+  }
 
-    void Update() {
-        //
-    }
+  void Update() {
+    //
+  }
 }
