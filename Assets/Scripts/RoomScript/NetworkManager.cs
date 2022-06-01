efcd4d5ad2e0319b530fa6417daca6588d847653
MJ Shin <ohgree.dev@gmail.com>
2022-06-01 22:32:03 +0900

    Revert "Lobby UI"

--- Assets/Scripts/RoomScript/NetworkManager.cs
+++ Assets/Scripts/RoomScript/NetworkManager.cs
@@ -1,29 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
+using Cinemachine;
 
-public class NetworkManager : MonoBehaviourPunCallbacks
-{
-    public CinemachineFreeLook cam;
-    void Start() {
-        //
-    }
+public class NetworkManager : MonoBehaviourPunCallbacks {
+  public CinemachineFreeLook cam;
+  void Start() {
+    PhotonNetwork.ConnectUsingSettings();
+  }
 
-    public override void OnJoinedRoom() {
-        GameObject player = PhotonNetwork.Instantiate("Player", new Vector3(0, 1, 10), Quaternion.identity);
-        cam.LookAt = cam.Follow = player.transform;
-    }
+  public override void OnConnectedToMaster() {
+    PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 20 }, null);
+  }
 
-    public void Disconnect() {
-        PhotonNetwork.LeaveRoom();
-        SceneManager.LoadScene("Lobby");
-    }
+  public override void OnJoinedRoom() {
+    GameObject player = PhotonNetwork.Instantiate("Player", new Vector3(0, 1, 10), Quaternion.identity);
+    cam.LookAt = cam.Follow = player.transform;
+  }
 
-    void Update() {
-        //
-    }
+  public void Disconnect() {
+    PhotonNetwork.Disconnect();
+  }
+
+  void Update() {
+    //
+  }
 }
