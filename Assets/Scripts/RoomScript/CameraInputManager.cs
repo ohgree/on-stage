ddf3d2be50266ac5edd670c5e7686c497f19fee0
MJ Shin <ohgree.dev@gmail.com>
2022-06-05 20:31:37 +0900

    Implement multiple camera perspectives
    Changelog
    ======
     - Add support for stage-bound cameras
     - Pressing `z` button will toggle between TPS camera and the selected
       stage cameras.
    
    Detailed Implementations
    ======
     - N/A
    
    Additional Notes & Issues
    ======
     - N/A

---
+++ Assets/Scripts/RoomScript/CameraInputManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraInputManager : MonoBehaviour {
+  public List<CinemachineVirtualCameraBase> cams;
+  int activeIndex = -1;
+  bool show = false;
+
+  // Start is called before the first frame update
+  void Start() {
+    cams.ForEach(cam => cam.enabled = false);
+    activeIndex = (++activeIndex) % cams.Count;
+    cams[activeIndex].enabled = true;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (Input.GetButtonDown("Show Next Camera")) {
+      Debug.Log("Show next cam");
+      ShowNextCam();
+    }
+  }
+
+  void ShowNextCam() {
+    cams[activeIndex].enabled = false;
+    activeIndex = (++activeIndex) % cams.Count;
+    cams[activeIndex].enabled = true;
+  }
+}
